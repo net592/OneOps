e95b5f64e726899c1b6cecbc3d788ec24d20da5a
net592 <net592@qq.com>
2016-04-22 16:09:38 +0800

    up

--- code_update/views.py
+++ code_update/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+
+#定义客户端状态页面
+@login_required(login_url="/account/login/")
+def code_up(request):
+    status = Minions_status.objects.all()
+    return render(request, 'code_update/codeup.html', {'status': status})
 
 # Create your views here.
